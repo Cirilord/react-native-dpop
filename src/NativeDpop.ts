fa33b633ea08f9a49f7ce6975203ebfd923bc497
Pedro Cirilo <phscirilo123@gmail.com>
2026-03-05 14:12:35 -0300

    feat: migrate dpop implementation to library module

--- src/NativeDpop.ts
+++ src/NativeDpop.ts
@@ -1,7 +1,30 @@
-import { TurboModuleRegistry, type TurboModule } from 'react-native';
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
+import type { UnsafeObject } from 'react-native/Libraries/Types/CodegenTypes';
 
 export interface Spec extends TurboModule {
-  multiply(a: number, b: number): number;
+  assertHardwareBacked(alias: string | null): Promise<void>;
+  calculateThumbprint(alias: string | null): Promise<string>;
+  deleteKeyPair(alias: string | null): Promise<void>;
+  getKeyInfo(alias: string | null): Promise<UnsafeObject>;
+  getPublicKeyDer(alias: string | null): Promise<string>;
+  getPublicKeyJwk(alias: string | null): Promise<UnsafeObject>;
+  getPublicKeyRaw(alias: string | null): Promise<string>;
+  hasKeyPair(alias: string | null): Promise<boolean>;
+  isBoundToAlias(proof: string, alias: string | null): Promise<boolean>;
+  rotateKeyPair(alias: string | null): Promise<void>;
+  signWithDpopPrivateKey(payload: string, alias: string | null): Promise<string>;
+  generateProof(
+    htu: string,
+    htm: string,
+    nonce: string | null,
+    accessToken: string | null,
+    additional: UnsafeObject | null,
+    kid: string | null,
+    jti: string | null,
+    iat: number | null,
+    alias: string | null
+  ): Promise<UnsafeObject>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('Dpop');
