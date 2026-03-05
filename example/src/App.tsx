b307de5982e0665e40d8d87640d4ff2e2fe82568
Pedro Cirilo <phscirilo123@gmail.com>
2026-03-05 14:13:17 -0300

    chore: update example app and workspace files

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,40 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-dpop';
-
-const result = multiply(3, 7);
+import { Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { DPoP } from 'react-native-dpop';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const onPress = async () => {
+    const dpop = await DPoP.generateProof({
+      htm: 'GET',
+      htu: 'https://api.example.com/resource',
+      kid: '123',
+    });
+
+    const thumbprint = await dpop.calculateThumbprint();
+
+    console.log(dpop, dpop.proof, thumbprint);
+    console.log(await dpop.getPublicKey('DER'));
+    console.log(await dpop.getPublicKey('JWK'));
+    console.log(await dpop.getPublicKey('RAW'));
+    console.log(await dpop.signWithDpopPrivateKey('RAW'));
+    console.log(await dpop.isBoundToAlias());
+    console.log(await DPoP.getKeyInfo());
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableHighlight onPress={onPress}>
+        <Text>Teste</Text>
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
+    backgroundColor: 'white',
+    flex: 1,
     justifyContent: 'center',
   },
 });
